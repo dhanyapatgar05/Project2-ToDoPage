7655252b13d90e4b4a3ac07fb522247950f19167
dhanyapatgar05 <dhanyapatgar05@gmail.com>
2025-03-04 16:27:08 +0530

    Initial commit

--- my-app/src/Components/ToDoPage.jsx
+++ my-app/src/Components/ToDoPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaCheck } from "react-icons/fa";
+import { FaCheckCircle  } from "react-icons/fa";
 
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { RiCalendar2Line, RiTeamLine  } from "react-icons/ri";
@@ -44,36 +44,46 @@ const TodoPage = () => {
             <span className="progress-bar"><AiOutlineLoading3Quarters  className="progress-icon" /> 78%</span>
           </span>
         </div>
-      
-        <div className="completed-tasks">
-          <div className="task-item completed">
-            <div className="task-info">
-              <h4 className="task-name">Meeting with Client</h4>
-              <p className="task-description">Redesign motion graphic...</p>
-              <span className="task-time">Today - 11:25 PM</span>
-            </div>
-            <FaCheck className="task-check" />
-            <div className="task-team">
-              <img src="/team3.jpg" alt="Team Member" className="team-member" />
-              <img src="/team4.jpg" alt="Team Member" className="team-member" />
-            </div>
+        <div className="completed-section">
+          <div className="completed-header">
+            <h3 className="completed-title">Completed</h3>
+            <span className="view-more">View More</span>
           </div>
-          <div className="task-item completed">
-            <div className="task-info">
-              <h4 className="task-name">Task Explorer NFT</h4>
-              <p className="task-description">Explore design mobile UI...</p>
-              <span className="task-time">01 July - 10:30 AM</span>
+        
+          <div className="task-content">
+            <div className="task-header">
+              <div className="task-name">Meeting with Client</div>
+              <FaCheckCircle  className="task-check" />
             </div>
-            <FaCheck className="task-check" />
-            <div className="task-team">
-              <img src="/team5.jpg" alt="Team Member" className="team-member" />
-              <img src="/team6.jpg" alt="Team Member" className="team-member" />
+            <p className="task-description">Redesign motion graphic...</p>
+            <div className="line">__________________________________________</div>
+            <div className="task-footer">
+              <span className="task-time">Today - 11:25 PM</span>
+              <div className="task-team">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTifunhA6ltTu6tVc5-fOv2g7SwTf-dUW89c5Q5eRygq53GmBb1vm5_Kurx-Mg1NwbzREU&usqp=CAU" alt="Team Member" className="team-member" />
+                <img src="https://cdn4.iconfinder.com/data/icons/professional-avatar-5/48/teacher_avatar_woman_women_profile_character_professions_2-512.png" alt="Team Member" className="team-member" />
+              </div>
             </div>
           </div>
         </div>
-        <button className="create-task-button">+ Create New</button>
+        <div className="task-content">
+          <div className="task-header">
+            <div className="task-name">Task Explorer NFT</div>
+              <FaCheckCircle  className="task-check" />
+            </div>
+            <p className="task-description">Explore design mobile UI...</p>
+            <div className="line">__________________________________________</div>
+            <div className="task-footer">
+              <span className="task-time">01 July - 10:30 AM</span>
+              <div className="task-team">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWBqBKkB60hEtGe0eS-K0FM1NSLesNcQwJNg&s" alt="Team Member" className="team-member" />
+                <img src="https://png.pngtree.com/png-clipart/20231017/original/pngtree-3d-employee-video-chat-png-image_13340394.png" alt="Team Member" className="team-member" />
+              </div>
+            </div>
+          </div>
+          <button className="create-task-button">+ Create New</button>
+        </div>
       </div>
-    </div>
   );
 };
 
